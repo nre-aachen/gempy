d839271706778da68d84abf5c9811405ceb05acb
Leguark <leguiom@gmail.com>
2016-12-22 14:01:47 +0100

    Docs 0.98
    This may be the last one?

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,8 +20,8 @@ import os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('/home/miguel/PycharmProjects/GeMpy/GeMpy'))
-sys.path.insert(1, os.path.abspath('../../GeMpy'))
-sys.path.insert(1, os.path.abspath('../GeMpy'))
+sys.path.insert(0, os.path.abspath('../../GeMpy'))
+
 
 # -- General configuration ------------------------------------------------
 
