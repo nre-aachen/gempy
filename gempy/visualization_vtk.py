6b9bc1b438f805f5cf30812d098e41bf3ef45879
Alex <alexander.schaaf@rwth-aachen.de>
2017-05-31 11:00:47 +0200

    VTK Visualization  - "5" now toggles between Actor and Camera interactor

--- gempy/visualization_vtk.py
+++ gempy/visualization_vtk.py
@@ -1,6 +1,7 @@
 import vtk
 import random
 import sys
+import numpy as np
 
 class InterfaceSphere(vtk.vtkSphereSource):
     def __init__(self, index):
@@ -12,24 +13,35 @@ class FoliationArrow(vtk.vtkArrowSource):
         self.index = index  # df index
 
 
-class CustomInteractor(vtk.vtkInteractorStyleTrackballActor):
+class CustomInteractorActor(vtk.vtkInteractorStyleTrackballActor):
     """
     Modified vtkInteractorStyleTrackballActor class to accomodate for interface df modification.
     """
-
-    def __init__(self, ren_list, geo_data, parent=None):
+    def __init__(self, ren_list, geo_data, parent):
+        self.parent = parent
         self.ren_list = ren_list
         self.geo_data = geo_data
-        self.AddObserver("MiddleButtonPressEvent", self.middleButtonPressEvent)
-        self.AddObserver("MiddleButtonReleaseEvent", self.middleButtonReleaseEvent)
+        self.AddObserver("MiddleButtonPressEvent", self.middle_button_press_event)
+        self.AddObserver("MiddleButtonReleaseEvent", self.middle_button_release_event)
 
-        self.AddObserver("LeftButtonPressEvent", self.leftButtonPressEvent)
-        self.AddObserver("LeftButtonReleaseEvent", self.leftButtonReleaseEvent)
+        self.AddObserver("LeftButtonPressEvent", self.left_button_press_event)
+        self.AddObserver("LeftButtonReleaseEvent", self.left_button_release_event)
+
+        self.AddObserver("KeyPressEvent", self.key_down_event)
 
         self.PickedActor = None
         self.PickedProducer = None
 
-    def leftButtonPressEvent(self, obj, event):
+    def key_down_event(self, obj, event):
+        iren = self.GetInteractor()
+        if iren is None:
+            return
+
+        key = iren.GetKeyCode()
+        if key == "5":  # switch to other renderer
+            self.parent.SetInteractorStyle(CustomInteractorCamera(self.ren_list, self.geo_data, self.parent))
+
+    def left_button_press_event(self, obj, event):
         print("Pressed left mouse button")
 
         m = vtk.vtkMatrix4x4()
@@ -49,19 +61,24 @@ class CustomInteractor(vtk.vtkInteractorStyleTrackballActor):
         # if self.PickedActor is
         # self.PickedActor.SetScale(2)
         # renwin.Render()
-
-        orientation = self.PickedActor.GetOrientation()
-        print(str(orientation))
+        try:
+            orientation = self.PickedActor.GetOrientation()
+            print(str(orientation))
+        except AttributeError:
+            pass
 
         self.OnLeftButtonDown()
 
-    def leftButtonReleaseEvent(self, obj, event):
+    def left_button_release_event(self, obj, event):
         # matrix = self.PickedActor.GetMatrix(vtk.vtkMatrix4x4())
-        matrix = self.PickedActor.GetOrientation()
-        print(str(matrix))
+        try:
+            matrix = self.PickedActor.GetOrientation()
+            print(str(matrix))
+        except AttributeError:
+            pass
         self.OnLeftButtonUp()
 
-    def middleButtonPressEvent(self, obj, event):
+    def middle_button_press_event(self, obj, event):
         # print("Middle Button Pressed")
         clickPos = self.GetInteractor().GetEventPosition()
 
@@ -91,7 +108,7 @@ class CustomInteractor(vtk.vtkInteractorStyleTrackballActor):
         self.OnMiddleButtonDown()
         return
 
-    def middleButtonReleaseEvent(self, obj, event):
+    def middle_button_release_event(self, obj, event):
         # print("Middle Button Released")
         if self.PickedActor is not None or type(self.PickedProducer) is not FoliationArrow:
             try:
@@ -109,85 +126,169 @@ class CustomInteractor(vtk.vtkInteractorStyleTrackballActor):
         return
 
 
-def visualize(geo_data):
+class CustomInteractorCamera(vtk.vtkInteractorStyleTrackballCamera):
+    def __init__(self, ren_list, geo_data, parent):
+        self.parent = parent
+        self.AddObserver("LeftButtonPressEvent", self.left_button_press_event)
+        self.AddObserver("LeftButtonReleaseEvent", self.left_button_release_event)
+        # self.AddObserver("MouseMoveEvent", self.mouse_move_event)
+
+        self.AddObserver("KeyPressEvent", self.key_down_event)
+
+        self.ren_list = ren_list
+        self.geo_data = geo_data
+        self.prev_mouse_pos = None
+
+        self.left_button_hold = False
+
+    def key_down_event(self, obj, ev):
+        iren = self.GetInteractor()
+        if iren is None: return
+
+        key = iren.GetKeyCode()
+        if key == "5":  # switch to other renderer
+            self.parent.SetInteractorStyle(CustomInteractorActor(self.ren_list, self.geo_data, self.parent))
+
+    def left_button_press_event(self, obj, ev):
+        self.left_button_hold = True
+        click_pos = self.GetInteractor().GetEventPosition()
+        # self.parent.SetCurrentRenderer(self.ren_list[0])
+
+        if click_pos[0] < 600:
+            self.OnLeftButtonDown()
+        else:
+            pass
+
+    def left_button_release_event(self, obj, ev):
+        self.left_button_hold = False
+        self.OnLeftButtonUp()
+
+    def mouse_move_event(self, obj, ev):
+        mouse_pos = self.GetInteractor().GetEventPosition()
+
+        if self.prev_mouse_pos is not None:
+            dx = mouse_pos[0] - self.prev_mouse_pos[0]
+            if mouse_pos[0] + dx >= 600:
+                self.left_button_release_event(obj, ev)
+            else:
+                self.OnMouseMove()
+        self.prev_mouse_pos = mouse_pos
+
+
+def visualize(geo_data, verbose=0):
     """
     Returns:
     """
     n_ren = 4
 
-    spheres = create_interface_spheres(geo_data)
+    # get model extent and calculate parameters for camera and sphere size
+    _e = geo_data.extent  # array([ x, X,  y, Y,  z, Z])
+    _e_dx = _e[1] - _e[0]
+    _e_dy = _e[3] - _e[2]
+    _e_dz = _e[5] - _e[4]
+    _e_d_avrg = (_e_dx + _e_dy + _e_dz) / 3
+    _e_max = np.argmax(geo_data.extent)
+
+    # create interface SphereSource
+    spheres = create_interface_spheres(geo_data, r=_e_d_avrg/30)
+    # create foliation ArrowSource
     arrows = create_foliation_arrows(geo_data)
+    # create arrow transformer
     arrows_transformers = create_arrow_transformers(arrows, geo_data)
 
+    # create mappers and actors for interface spheres and foliation arrows
     mappers, actors = create_mappers_actors(spheres)
     arrow_mappers, arrow_actors = create_mappers_actors(arrows_transformers)
 
+    # create render window, settings
     renwin = vtk.vtkRenderWindow()
     renwin.SetSize(1000, 800)
-    renwin.SetWindowName('Render Window')
+    renwin.SetWindowName('GeMpy 3D-Editor')
 
-    xmins = [0, 0.4, 0.4, 0.4]
-    xmaxs = [0.4, 1, 1, 1]
+    # viewport dimensions setup
+    xmins = [0, 0.6, 0.6, 0.6]
+    xmaxs = [0.6, 1, 1, 1]
     ymins = [0, 0, 0.33, 0.66]
     ymaxs = [1, 0.33, 0.66, 1]
 
+    # create list of renderers, set vieport values
     ren_list = []
     for i in range(n_ren):
         ren_list.append(vtk.vtkRenderer())
         renwin.AddRenderer(ren_list[-1])
         ren_list[-1].SetViewport(xmins[i], ymins[i], xmaxs[i], ymaxs[i])
 
+    # //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    # create interactor and set interactor style, assign render window
     interactor = vtk.vtkRenderWindowInteractor()
-    interactor.SetInteractorStyle(CustomInteractor(ren_list, geo_data))
+    interactor.SetInteractorStyle(CustomInteractorCamera(ren_list, geo_data, interactor))
     interactor.SetRenderWindow(renwin)
 
-    _e = geo_data.extent  # array([ x, X,  y, Y,  z, Z])
-
+    # //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     # 3d model camera
     model_cam = vtk.vtkCamera()
-    model_cam.SetPosition(_e[1]*5, _e[3]*5, _e[5]*5)
-    model_cam.SetFocalPoint(_e[1]/2, _e[3]/2, _e[5]/2)
+    model_cam.SetPosition(_e[_e_max]*5, _e[_e_max]*5, _e[_e_max]*5)
+    model_cam.SetFocalPoint(np.min(_e[0:2]) + _e_dx / 2,
+                            np.min(_e[2:4]) + _e_dy / 2,
+                            np.min(_e[4:]) + _e_dz / 2)
 
-    # XY camera
+    # XY camera RED
     xy_cam = vtk.vtkCamera()
-    xy_cam.SetPosition(_e[1] / 2,
-                       _e[3] / 2,
-                       _e[5] * 3)
+    # if np.argmin(_e[4:]) == 0:
+    xy_cam.SetPosition(np.min(_e[0:2]) + _e_dx / 2,
+                       np.min(_e[2:4]) + _e_dy / 2,
+                       _e[_e_max] * 4)
+    # else:
+    #    xy_cam.SetPosition(np.min(_e[0:2]) + _e_dx / 2,
+    #                       np.min(_e[2:4]) + _e_dy / 2,
+    #                       _e[_e_max] * 4)
 
-    xy_cam.SetFocalPoint(_e[1] / 2,
-                         _e[3] / 2,
-                         _e[5] / 2)
+    xy_cam.SetFocalPoint(np.min(_e[0:2]) + _e_dx / 2,
+                         np.min(_e[2:4]) + _e_dy / 2,
+                         np.min(_e[4:]) + _e_dz / 2)
 
-    # YZ camera
+    # YZ camera GREEN
     yz_cam = vtk.vtkCamera()
-    yz_cam.SetPosition(_e[1] * 3,
-                       _e[3] / 2,
-                       _e[5] / 2)
+    yz_cam.SetPosition(_e[_e_max] * 4,
+                       np.min(_e[2:4]) + _e_dy / 2,
+                       np.min(_e[4:]) + _e_dz / 2)
 
-    yz_cam.SetFocalPoint(_e[1] / 2,
-                         _e[3] / 2,
-                         _e[5] / 2)
+    yz_cam.SetFocalPoint(np.min(_e[0:2]) + _e_dx / 2,
+                         np.min(_e[2:4]) + _e_dy / 2,
+                         np.min(_e[4:]) + _e_dz / 2)
 
-    # XZ camera
+    # XZ camera BLUE
     xz_cam = vtk.vtkCamera()
-    xz_cam.SetPosition(_e[1] / 2,
-                       _e[3] * 3,
-                       _e[5] / 2)
+    xz_cam.SetPosition(np.min(_e[0:2]) + _e_dx / 2,
+                       _e[_e_max] * 4,
+                       np.min(_e[4:]) + _e_dz / 2)
 
-    xz_cam.SetFocalPoint(_e[1] / 2,
-                         _e[3] / 2,
-                         _e[5] / 2)
+    xz_cam.SetFocalPoint(np.min(_e[0:2]) + _e_dx / 2,
+                         np.min(_e[2:4]) + _e_dy / 2,
+                         np.min(_e[4:]) + _e_dz / 2)
     xz_cam.SetViewUp(1, 0, 0)
 
+    # camera position debugging
+    if verbose == 1:
+        print("RED XY:", xy_cam.GetPosition())
+        print("RED FP:", xy_cam.GetFocalPoint())
+        print("GREEN YZ:", yz_cam.GetPosition())
+        print("GREEN FP:", yz_cam.GetFocalPoint())
+        print("BLUE XZ:", xz_cam.GetPosition())
+        print("BLUE FP:", xz_cam.GetFocalPoint())
+
     camera_list = [model_cam, xy_cam, yz_cam, xz_cam]
+    ren_color = [(0,0,0), (0.5,0.,0.1), (0.1,0.5,0.1), (0.1,0.1,0.5)]
 
     for i in range(n_ren):
         ren_list[i].SetActiveCamera(camera_list[i])
+        ren_list[i].SetBackground(ren_color[i][0], ren_color[i][1], ren_color[i][2])
 
-    # ///////////////////////////////////////////////////////////////
+    # //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     # create AxesActor and customize
     cube_axes_actor = create_axes(geo_data, camera_list)
 
+    # add interface and foliation actors to all renderers
     for r in ren_list:
         # add axes actor to all renderers
         r.AddActor(cube_axes_actor)
@@ -197,6 +298,7 @@ def visualize(geo_data):
         for a in arrow_actors:
             r.AddActor(a)
 
+    # initialize and start the app
     interactor.Initialize()
     interactor.Start()
 
@@ -303,11 +405,13 @@ def create_arrow_transformers(arrows, geo_data):
     return arrows_transformers
 
 
-def create_axes(geo_data, camera_list):
+def create_axes(geo_data, camera_list, verbose=0):
     "Create and return cubeAxesActor, settings."
     cube_axes_actor = vtk.vtkCubeAxesActor()
     cube_axes_actor.SetBounds(geo_data.extent)
-    cube_axes_actor.SetCamera(camera_list[0])
+    cube_axes_actor.SetCamera(camera_list[1])
+    if verbose == 1:
+        print(cube_axes_actor.GetAxisOrigin())
 
     # set axes and label colors
     cube_axes_actor.GetTitleTextProperty(0).SetColor(1.0, 0.0, 0.0)
