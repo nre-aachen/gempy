6e3dc2cd71eedf1685b33f50eff032eb35f9ff22
Alex <alexander.schaaf@rwth-aachen.de>
2017-07-03 10:34:39 +0200

    getting things ready for PyPi - license - setup.cfg - setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.9',
     packages=['gempy'],
     url='https://github.com/nre-aachen/GeMpy',
+    download_url='',  # needs git tag first
     author='Miguel de la Varga',
     author_email='varga@aices.rwth-aachen.de',
     description='3D implicit structural modeling library for Python.',
